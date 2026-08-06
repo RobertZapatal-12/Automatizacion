1ff213f3625630dd776dc9c605b314cd304e2d7d
RobertZapatal-12 <yarelzapatal@gmail.com>
2026-08-06 12:42:32 -0400

    Implement main application structure with GUI for CSV file selection and information display

--- interfaz.py
+++ interfaz.py
@@ -1,9 +1,23 @@
 import tkinter as tk
+from read import abrir
+from tkinter import filedialog
 
-ventana = tk.Tk()
-ventana.title("Lector y limpieza de csv")
-ventana.geometry("600x400")
+def root():
+    root = tk.Tk()
+    root.title("Doc cleaner")
+    root.geometry("600x400")
+
+    button_selectcsv = tk.Button(root, text="Selecciona un documento", command=abrir)
+    button_selectcsv.pack()
 
 
+    widget_showcsv = tk.LabelFrame(root, text="Informaion del archivo", padx= 5, pady= 5)
+    widget_showcsv.pack()
 
-ventana.mainloop()
+    tk.Label(widget_showcsv, text="Archivo:").pack()
+    tk.Label(widget_showcsv, text="Filas:").pack()
+    tk.Label(widget_showcsv, text="Columnas:").pack()
+
+    # checkbutton = tk.Checkbutton(ventana, text= "Limpiar nulos")
+    # checkbutton.grid()
+    root.mainloop()
--- read.py
+++ read.py
@@ -1,2 +1,19 @@
 import pandas as pd
+import tkinter as tk
+from tkinter import filedialog
+
+def abrir():
+    archivo = tk.filedialog.askopenfile(mode = "r")
+    print(f"Archivo cargado correctamente")
+
+def limpiar_nulos():
+    df = pd.read_csv("archivo")
+    df.dropna()
+
+
+
+
+
+    
+
 
